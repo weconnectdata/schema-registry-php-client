d4185cf304711db23adc72585b197754b08ac5a3
Thomas Ploch <thomas.ploch@flixbus.com>
2020-10-04 07:06:51 +0200

    Adding `permanent` flag to `deleteSubject` Request

--- test/RequestsTest.php
+++ test/RequestsTest.php
@@ -333,10 +333,10 @@ class RequestsTest extends TestCase
      */
     public function it_should_produce_a_valid_subject_deletion_request(): void
     {
-        $request = Requests::deleteSubjectRequest('test');
+        $request = Requests::deleteSubjectRequest('test', false);
 
         self::assertEquals('DELETE', $request->getMethod());
-        self::assertEquals('/subjects/test', $request->getUri());
+        self::assertEquals('/subjects/test?permanent=false', $request->getUri());
         self::assertEquals([Constants::ACCEPT => [Constants::ACCEPT_HEADER[Constants::ACCEPT]]], $request->getHeaders());
     }
 
